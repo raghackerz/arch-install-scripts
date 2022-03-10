b52453dfc4d1f16b9690414198b5e2b0d75bfa0f
Raghvendra Kumar Singh <raghvendra14fb@gmail.com>
2022-03-10 12:42:23 +0530

    made corrections

--- xmonad.sh
+++ xmonad.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+cd $HOME
+
 #xorg, wm and compositor
 sudo pacman -S xorg xorg-xinit xmonad xmonad-contrib xmobar picom feh
 
 #basic apps
-sudo pacman -S dmenu dunst exa fzf git htop mpv ncdu neofetch neovim pass qalculate-gtk tldr unzip wget which zathura zathura-pdf-mupdf zathura-pdf-poppler zoxide zsh zsh-completion
+sudo pacman -S dmenu dunst exa fzf git htop mpv ncdu neofetch neovim pass qalculate-gtk tldr unzip wget which zathura zathura-pdf-poppler zoxide zsh zsh-completions
 
 #development apps
 #openjdk-jdk
@@ -15,15 +17,15 @@ sudo mkdir -p /usr/local/share/fonts/otf/
 sudo mkdir -p /usr/local/share/fonts/ttf/
 
 wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Iosevka.zip" -O Iosevka.zip
-uzip Iosevka.zip -d Iosevka
+unzip Iosevka.zip -d Iosevka
 rm Iosevka/*Windows*
-sudo cp -R ./Iosevka /usr/share/fonts/ttf/Iosevka
+sudo cp -R ./Iosevka /usr/share/fonts/ttf/
 rm -rf Iosevka
 
 wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/CodeNewRoman.zip" -O CodeNewRoman.zip
-uzip CodeNewRoman.zip -d CodeNewRoman
+unzip CodeNewRoman.zip -d CodeNewRoman
 rm CodeNewRoman/*Windows*
-sudo cp -R ./CodeNewRoman /usr/share/fonts/ttf/CodeNewRoman
+sudo cp -R ./CodeNewRoman /usr/share/fonts/otf/
 rm -rf CodeNewRoman
 
 fc-cache -fv
@@ -37,7 +39,11 @@ sudo make install
 cd
 
 #copy dotfiles
-git clone https://github.com/raghackerz/dotfiles-personal.git $HOME/.dotfiles/
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-dotfiles config --local status.showUntrackedFiles no
-dotfiles checkout
+git clone --bare https://github.com/raghackerz/dotfiles-personal.git $HOME/.dotfiles/
+/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
+/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
+
+chmod +x $HOME/.scripts/*
+chsh -s /usr/bin/zsh
+
